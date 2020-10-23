435cbc69caeb615861e64433afa5fdf1f8d7b367
r.cody <ryan.p.cody@gmail.com>
2020-10-23 14:36:40 -0400

    renaming and refactoring

---
+++ MoonGate/Events/EventPublisher.cs
@@ -0,0 +1,51 @@
+﻿using Network;
+using Network.Controllers;
+using Network.Factory;
+using System;
+
+namespace Moongate.Network.Events
+{
+	public class EventPublisher : EventPublisherBase
+	{
+		readonly ILogger logger;
+		readonly IHandlerFactory handlerFactory;
+		readonly GameStateController gameStateController;
+
+		public override EventHandler<PingArgs> PingReceived { get; set; }
+		public override EventHandler<EntityArgs> EntityAdded { get; set; }
+		public override EventHandler<EntityArgs> EntityUpdated { get; set; }
+		public override EventHandler<ControlArgs> PlayerInputChanged { get; set; }
+
+		public EventPublisher (ILogger _logger, IHandlerFactory _handlerFactory, GameStateController _gameStateController) 
+		{
+			logger = _logger;
+			handlerFactory = _handlerFactory;
+			gameStateController = _gameStateController;
+
+			((PingHandler)handlerFactory.GetHandler(MessageType.Ping)).PingReceived += OnPingReceived;
+			gameStateController.EntityAdded += OnEntityAdded;
+			gameStateController.EntityUpdated += OnEntityUpdated;
+			((PlayerInputHandler)handlerFactory.GetHandler(MessageType.PlayerInput)).PlayerInputChanged += OnPlayerInputChanged;
+		}
+
+		public override void OnPlayerInputChanged (object sender, ControlArgs args)
+		{
+			PlayerInputChanged?.Invoke(this, args);
+		}
+
+		public override void OnPingReceived (object sender, PingArgs args)
+		{
+			PingReceived?.Invoke(this, args);
+		}
+
+		public override void OnEntityAdded (object sender, EntityArgs args)
+		{
+			EntityAdded?.Invoke(this, args);
+		}
+
+		public override void OnEntityUpdated (object sender, EntityArgs args)
+		{
+			EntityUpdated?.Invoke(this, args);
+		}
+	}
+}
---
+++ MoonGate/Events/EventPublisherBase.cs
@@ -0,0 +1,21 @@
+﻿using Network.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moongate.Network.Events
+{
+	public abstract class EventPublisherBase
+	{
+		public abstract EventHandler<PingArgs> PingReceived { get; set; }
+		public abstract EventHandler<EntityArgs> EntityAdded { get; set; }
+		public abstract EventHandler<EntityArgs> EntityUpdated { get; set; }
+		public abstract EventHandler<ControlArgs> PlayerInputChanged { get; set; }
+
+		public abstract void OnPingReceived (object sender, PingArgs args);
+		public abstract void OnEntityAdded (object sender, EntityArgs args);
+		public abstract void OnEntityUpdated (object sender, EntityArgs args);
+		public abstract void OnPlayerInputChanged (object sender, ControlArgs args);
+
+	}
+}
