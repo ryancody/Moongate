7e618169ce32eaa759c3fd09c3a795bd79bf138e
Ryan <8892867+ryancody@users.noreply.github.com>
2022-01-08 23:01:11 -0500

    remove unit tests, they need rewriting.  add server commands and update framework app for unity build (#5)

---
+++ Moongate.App.Framework/IncorrectAgentError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Moongate.App
+{
+	public class IncorrectAgentError : Exception
+	{
+
+		public IncorrectAgentError (string message) : base(message)
+		{ 
+			
+		}
+	}
+}
